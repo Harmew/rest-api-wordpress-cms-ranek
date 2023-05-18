b636c6597ccc1717b87e09f8b604fb0fe3cd3373
Daniel De Oliveira <daniel.oliveira@liggavc.com.br>
2023-05-18 11:56:43 -0300

    correcao de nome de arquivos

--- endpoints/produto_post.php
+++ endpoints/produto_post.php
@@ -1,42 +1,42 @@
 <?php 
 
-function api_usuario_put($request) {
+function api_produto_post($request) {
     $user = wp_get_current_user();
     $user_id = $user->ID;
 
     if($user_id > 0) {
-        $email = sanitize_email($request['email']);
         $nome = sanitize_text_field($request['nome']);
-        $senha = $request['senha'];
-        $cep = sanitize_text_field($request['cep']);
-        $rua = sanitize_text_field($request['rua']);
-        $numero = sanitize_text_field($request['numero']);
-        $cidade = sanitize_text_field($request['cidade']);
-        $bairro = sanitize_text_field($request['bairro']);
-        $estado = sanitize_text_field($request['estado']);
+        $preco = sanitize_text_field($request['preco']);
+        $descricao = sanitize_text_field($request['descricao']);
+        $usuario_id = $user->user_login;
 
-        $email_exists = email_exists($email);
+        $response = array(
+            'post_author' => $user_id,
+            'post_type' => 'produto',
+            'post_title' => $nome,
+            'post_status' => 'publish',
+            'meta_input' => array(
+                'nome' => $nome,
+                'preco' => $preco,
+                'descricao' => $descricao,
+                'usuario_id' => $usuario_id,
+                'vendido' => 'false',
+            ),
+        );
 
-        if(!$email_exists || $email_exists === $user_id) {
-            
-            $response = array(
-                'ID' => $user_id,
-                'user_pass' => $senha,
-                'user_email' => $email,
-                'display_name' => $nome,
-                'first_name' => $nome,
-            );
+        $produto_id = wp_insert_post($response);
+        $response['id'] = get_post_field('post_name', $produto_id);
 
-            wp_update_user($response);
+        $files = $request->get_file_params();
 
-            update_user_meta($user_id,'cep', $cep);
-            update_user_meta($user_id,'rua', $rua);
-            update_user_meta($user_id,'numero',$numero);
-            update_user_meta($user_id,'cidade',$cidade);
-            update_user_meta($user_id,'bairro',$bairro);
-            update_user_meta($user_id,'estado',$estado);
-        } else {
-            $response = new WP_Error('email', 'Email já cadastrado.', array('status' => 403));
+        if(files) {
+            require_once(ABSPATH . 'wp-admin/includes/image.php');
+            require_once(ABSPATH . 'wp-admin/includes/file.php');
+            require_once(ABSPATH . 'wp-admin/includes/media.php');
+
+            foreach($files as $file => $array) {
+                media_handle_upload($file, $produto_id);
+            }
         }
 
     } else {
@@ -46,17 +46,17 @@ function api_usuario_put($request) {
     return rest_ensure_response($response);
 }
 
-function registrar_api_usuario_put() {
-    register_rest_route('api', '/usuario', 
+function registrar_api_produto_post() {
+    register_rest_route('api', '/produto', 
         array(
             array(
-                'methods' => WP_REST_Server::CREATABLE,
-                'callback' => 'api_usuario_put',
+                'methods' => WP_REST_Server::EDITABLE,
+                'callback' => 'api_produto_post',
             ),
         )
     );
 }
 
-add_action('rest_api_init', 'registrar_api_usuario_put');
+add_action('rest_api_init', 'registrar_api_produto_post');
 
 ?>
